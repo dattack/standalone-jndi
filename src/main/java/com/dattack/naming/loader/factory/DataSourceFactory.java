53dbbf1f6b053f876afe3c8c60518b3e4851709a
cvarela <cvarela@dattack.com>
2017-06-14 23:58:21 +0200

    Added support for managing encrypted passwords

--- src/main/java/com/dattack/naming/loader/factory/DataSourceFactory.java
+++ src/main/java/com/dattack/naming/loader/factory/DataSourceFactory.java
@@ -16,6 +16,8 @@
 package com.dattack.naming.loader.factory;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.security.PrivateKey;
 import java.util.Collection;
 import java.util.Properties;
 
@@ -23,6 +25,7 @@ import javax.naming.ConfigurationException;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.apache.commons.configuration.CompositeConfiguration;
 import org.apache.commons.configuration.ConfigurationConverter;
 import org.apache.commons.configuration.EnvironmentConfiguration;
@@ -32,8 +35,11 @@ import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.dattack.jtoolbox.io.FilesystemUtils;
 import com.dattack.jtoolbox.jdbc.DataSourceClasspathDecorator;
 import com.dattack.jtoolbox.jdbc.SimpleDataSource;
+import com.dattack.jtoolbox.security.DattackSecurityException;
+import com.dattack.jtoolbox.security.RsaUtils;
 
 /**
  * @author cvarela
@@ -43,44 +49,99 @@ public class DataSourceFactory implements ResourceFactory<DataSource> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceFactory.class);
 
+    private static final String ENCRYPT_PREFIX = "encrypt";
+    private static final String PRIVATE_KEY_FILENAME = "privateKey";
+    private static final String GLOBAL_PRIVATE_KEY_FILENAME = "globalPrivateKey";
+    private static final String DEFAULT_PRIVATE_KEY = "id_rsa";
+
     private static final String DRIVER_KEY = "driverClassName";
     private static final String URL_KEY = "url";
     private static final String USERNAME_KEY = "username";
     private static final String PASSWORD_KEY = "password";
     public static final String TYPE = "javax.sql.DataSource";
 
-    @Override
-    public DataSource getObjectInstance(final Properties properties, final Collection<File> extraClasspath)
-            throws NamingException {
-
+    private static AbstractConfiguration getConfiguration(final Properties properties) {
         final CompositeConfiguration configuration = new CompositeConfiguration();
         configuration.addConfiguration(new SystemConfiguration());
         configuration.addConfiguration(new EnvironmentConfiguration());
         configuration.addConfiguration(new MapConfiguration(properties));
+        return configuration;
+    }
 
-        final String driver = configuration.getString(DRIVER_KEY);
-        if (driver == null) {
-            throw new ConfigurationException(String.format("Missing property '%s'", DRIVER_KEY));
+    private static String getMandatoryProperty(final AbstractConfiguration configuration, final String propertyName)
+            throws ConfigurationException {
+
+        final String value = configuration.getString(propertyName);
+        if (value == null) {
+            throw new ConfigurationException(String.format("Missing property '%s'", propertyName));
         }
+        return value;
+    }
 
-        final String url = configuration.getString(URL_KEY);
-        if (url == null) {
-            throw new ConfigurationException(String.format("Missing property '%s'", URL_KEY));
-        }
+    private static String getPassword(final AbstractConfiguration configuration)
+            throws ConfigurationException, DattackSecurityException {
 
-        final String user = configuration.getString(USERNAME_KEY);
         final String password = configuration.getString(PASSWORD_KEY);
 
-        DataSource dataSource = null;
-        try {
-            final Properties props = ConfigurationConverter.getProperties(configuration);
-            dataSource = BasicDataSourceFactory.createDataSource(props);
-        } catch (final Exception e) { // NOPMD by cvarela on 8/02/16 22:28
-            // we will use a DataSource without a connection pool
-            LOGGER.info(e.getMessage());
-            dataSource = new SimpleDataSource(driver, url, user, password);
+        if (password != null && password.startsWith(ENCRYPT_PREFIX)) {
+            final PrivateKey privateKey = getPrivateKey(configuration);
+            final String encryptedPassword = password.substring(PASSWORD_KEY.length());
+            return new String(RsaUtils.decryptBase64(encryptedPassword.getBytes(Charset.defaultCharset()), privateKey),
+                    Charset.defaultCharset());
         }
 
-        return new DataSourceClasspathDecorator(dataSource, extraClasspath);
+        // not encrypted password
+        return password;
+    }
+
+    private static PrivateKey getPrivateKey(final AbstractConfiguration configuration)
+            throws DattackSecurityException, ConfigurationException {
+
+        String keyFilename = configuration.getString(PRIVATE_KEY_FILENAME);
+
+        if (keyFilename == null) {
+            keyFilename = configuration.getString(GLOBAL_PRIVATE_KEY_FILENAME);
+        }
+
+        if (keyFilename == null) {
+            keyFilename = FilesystemUtils.locateFile(DEFAULT_PRIVATE_KEY).getAbsolutePath();
+        }
+
+        // Guard: This should never happen because we have a default private-key
+        // to use
+        if (keyFilename == null) {
+            throw new ConfigurationException("Unable to find the private key. Check your configuration");
+        }
+
+        return RsaUtils.loadPrivateKey(keyFilename);
+    }
+
+    @Override
+    public DataSource getObjectInstance(final Properties properties, final Collection<File> extraClasspath)
+            throws NamingException {
+
+        try {
+            final AbstractConfiguration configuration = getConfiguration(properties);
+
+            final String driver = getMandatoryProperty(configuration, DRIVER_KEY);
+            final String url = getMandatoryProperty(configuration, URL_KEY);
+            final String plainPassword = getPassword(configuration);
+
+            DataSource dataSource = null;
+            try {
+                configuration.setProperty(PASSWORD_KEY, plainPassword);
+                final Properties props = ConfigurationConverter.getProperties(configuration);
+                dataSource = BasicDataSourceFactory.createDataSource(props);
+            } catch (final Exception e) { // NOPMD by cvarela on 8/02/16 22:28
+                // we will use a DataSource without a connection pool
+                LOGGER.info(e.getMessage());
+                final String user = configuration.getString(USERNAME_KEY);
+                dataSource = new SimpleDataSource(driver, url, user, plainPassword);
+            }
+
+            return new DataSourceClasspathDecorator(dataSource, extraClasspath);
+        } catch (final DattackSecurityException e) {
+            throw new SecurityConfigurationException(e);
+        }
     }
 }
