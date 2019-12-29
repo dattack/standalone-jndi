0b3f8772011d15228424e9d33dca8c1a20c95c27
cvarela <cvarela@dattack.com>
2019-12-29 18:43:51 +0100

    Travis configuration update

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_e0905f71ae75_key -iv $encrypted_e0905f71ae75_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-    gpg --fast-import .travis/codesigning.asc
-fi
+openssl aes-256-cbc -K $encrypted_e0905f71ae75_key -iv $encrypted_e0905f71ae75_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+gpg --fast-import .travis/codesigning.asc
